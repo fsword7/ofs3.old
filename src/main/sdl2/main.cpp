116553bf80889af92292e916f2b1a91b493c8dcb
Timothy Stark <fsword007@gmail.com>
2020-05-10 22:59:24 -0400

    Now handling SDL2 libraries

---
+++ src/main/sdl2/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp - SDL2-based main package
+ *
+ *  Created on: May 10, 2020
+ *      Author: Tim Stark
+ */
+
+//#include <SDL2/SDL.h>
+//#include <SDL2/SDL_image.h>
+
+#include "main/core.h"
+
+int main(int argc, char **argv)
+{
+	cout << "Orbital Flight Simulator" << endl;
+
+	return 0;
+}
+
+
+
+
