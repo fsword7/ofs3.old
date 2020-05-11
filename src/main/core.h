f70dcb292c24b754c702aa957c859b26828b8588
Timothy Stark <fsword007@gmail.com>
2020-05-10 22:27:10 -0400

    Merged core.h and math.h

--- src/main/core.h
+++ src/main/core.h
@@ -12,7 +12,35 @@
 #include <string>
 #include <vector>
 
-#include "main/math.h"
 #include "util/color.h"
+
+// Math library package
+
+#define PI (3.14159265358979323846)
+
+#include <cmath>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+typedef glm::tvec2<float> vec2f_t;
+typedef glm::tvec3<float> vec3f_t;
+typedef glm::tvec4<float> vec4f_t;
+typedef glm::tquat<float> quatf_t;
+
+typedef glm::fmat3 mat3f_t;
+typedef glm::fmat4 mat4f_t;
+
+typedef glm::tvec2<double> vec2d_t;
+typedef glm::tvec3<double> vec3d_t;
+typedef glm::tvec4<double> vec4d_t;
+typedef glm::tquat<double> quatd_t;
+
+typedef glm::dmat3 mat3d_t;
+typedef glm::dmat4 mat4d_t;
+
 
 using namespace std;
