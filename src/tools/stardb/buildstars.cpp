35aebca72d6701c2b0722740e9e797ef873e35be
Timothy Stark <fsword007@gmail.com>
2020-05-11 22:47:39 -0400

    Building star database for OFS program

---
+++ src/tools/stardb/buildstars.cpp
@@ -0,0 +1,18 @@
+/*
+ * buildstars.cpp - building star database file
+ *
+ *  Created on: May 11, 2020
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "universe/astro.h"
+#include "universe/xhipdata.h"
+
+using namespace ofs::astro;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
