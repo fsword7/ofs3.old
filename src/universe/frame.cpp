7e2f6adc660b5b2ee5133154083a98191faf3b50
Timothy Stark <fsword007@gmail.com>
2020-05-12 08:49:59 -0400

    Added reference frame package

---
+++ src/universe/frame.cpp
@@ -0,0 +1,13 @@
+/*
+ * frame.cpp - reference frame package
+ *
+ *  Created on: May 12, 2020
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "universe/frame.h"
+
+
+
+
---
+++ src/universe/frame.h
@@ -0,0 +1,36 @@
+/*
+ * frame.h - reference frame package
+ *
+ *  Created on: May 12, 2020
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+namespace ofs::universe
+{
+	class Object;
+
+	class ReferenceFrame
+	{
+	public:
+		ReferenceFrame() = default;
+		virtual ~ReferenceFrame() = default;
+
+		inline bool isRoot() const       { return parent == nullptr; }
+		inline Object *getCenter() const { return center; }
+
+	protected:
+		ReferenceFrame *parent = nullptr;
+		Object         *center = nullptr;
+	};
+
+	class CachingFrame : public ReferenceFrame
+	{
+	public:
+		CachingFrame() = default;
+		~CachingFrame() = default;
+
+	private:
+	};
+}
