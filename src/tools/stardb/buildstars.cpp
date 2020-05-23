2ecd0a9216e330ca9350b89e8ac7de2ebdcb4fd2
Timothy Stark <fsword007@gmail.com>
2020-05-23 18:36:47 -0400

    New star entry structure

--- src/tools/stardb/buildstars.cpp
+++ src/tools/stardb/buildstars.cpp
@@ -11,6 +11,16 @@
 
 using namespace ofs::astro;
 
+struct starEntry
+{
+	int hipNumber = -1;
+	int hdNumber = -1;
+
+	double ascension = 0.0;
+	double declination = 0.0;
+	double parallax = 0.0;
+};
+
 int main(int argc, char **argv)
 {
 
